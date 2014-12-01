6459d22e81b278f4af08a9ab573740a1d36e8197
VIIjordon@gmail.com <VIIJordon@gmail.com>
2014-12-01 13:27:00 -0800

    made changes to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
 	def index
-		@user = User.all
+		@users = User.all
 	end
 	
 	def new
@@ -19,11 +19,27 @@ class UsersController < ApplicationController
   end
 
   def show
+  	# @user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			redirect_to users_path
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
 		User.find(params[:id]).destroy
     	redirect_to root_path
 	end
+
+	private
+
+	def user_params
+    params.require(:user).permit(:email, :password, :password_digest)
+  end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User
   include Mongoid::Document
+  include Mongoid::Timestamps
+  field :first_name, type: String
+  field :last_name, type: String
+  field :dob, type: Date
   field :email, type: String
   field :password, type: String
   field :password_digest, type: String
+  field :role_id, type: String
 
-  has_many :roles
+  belongs :roles
   has_many :statuses
 end
